feb9d623bb177ac067e4e3bccd21c92656c84350
TomOw <owczarczyk.tomasz@wp.pl>
2018-08-15 13:54:39 +0200

    Planet list page layout

--- src/app/planets-material.module.ts
+++ src/app/planets-material.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule,
+    MatInputModule
   ],
   exports: [
     MatButtonModule,
+    MatInputModule
   ]
 })
 export class PlanetsMaterialModule {
